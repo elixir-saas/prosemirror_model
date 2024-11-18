652d1f781726d21871cf2b3478034bf1a60766bc
Justin Tormey <jrtormey@gmail.com>
2024-11-18 17:24:13 -0600

    Implement helper for extracting blocks by module

--- lib/prosemirror_model.ex
+++ lib/prosemirror_model.ex
@@ -8,4 +8,17 @@ defmodule ProsemirrorModel do
       import ProsemirrorModel.ModifierHelper
     end
   end
+
+  @doc """
+  Extracts blocks of a particular module from a document tree.
+  """
+  def extract_blocks(%block_module{} = block, block_module) do
+    [block]
+  end
+
+  def extract_blocks(%{content: content}, block_module) when is_list(content) do
+    Enum.flat_map(content, &extract_blocks(&1, block_module))
+  end
+
+  def extract_blocks(_otherwise, _block_module), do: []
 end
--- test/prosemirror_model_test.exs
+++ test/prosemirror_model_test.exs
@@ -1,3 +1,38 @@
 defmodule ProsemirrorModelTest do
   use ExUnit.Case
+
+  alias ProsemirrorModel.Block
+
+  describe "extract_blocks/2" do
+    document =
+      %{
+        content: [
+          %Block.Paragraph{
+            content: [
+              %Block.Heading{
+                attrs: %Block.Heading.Attrs{level: 1},
+                content: [%Block.Text{text: "h1"}]
+              },
+              %Block.Text{text: "Heading"}
+            ]
+          },
+          %Block.Paragraph{
+            content: [
+              %Block.Text{text: "Some leading text"},
+              %Block.Heading{
+                attrs: %Block.Heading.Attrs{level: 2},
+                content: [%Block.Text{text: "h2"}]
+              }
+            ]
+          }
+        ]
+      }
+
+    headings = [
+      %Block.Heading{attrs: %Block.Heading.Attrs{level: 1}, content: [%Block.Text{text: "h1"}]},
+      %Block.Heading{attrs: %Block.Heading.Attrs{level: 2}, content: [%Block.Text{text: "h2"}]}
+    ]
+
+    assert ^headings = ProsemirrorModel.extract_blocks(document, Block.Heading)
+  end
 end
